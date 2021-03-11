b5b0220ba98b2b41826870d8f7ddd773a42fd008
Chen Si <chensi3@illinois.edu>
2021-03-11 13:39:10 +0800

    import new data and display in a graph

--- Research/chensi3/weather_ML_test/weather_ML_test.py
+++ Research/chensi3/weather_ML_test/weather_ML_test.py
@@ -1,6 +1,6 @@
+#import and modify the raw data
 from pandas import read_csv
 from datetime import datetime
-
 
 def parse(x):
     return datetime.strptime(x, '%Y %m %d %H')
@@ -18,6 +18,28 @@ dataset['pollution'].fillna(0, inplace=True)
 dataset = dataset[24:]
 # summarize first 5 rows
 print(dataset.head(5))
-# save to file
+# save to "new_pollution.csv"
 new_url = "F:\\CS196_project\\Group13-SP21\\Research\\chensi3\\weather_ML_test\\new_pollution.csv"
 dataset.to_csv(new_url)
+
+
+
+#import new data and show them in a  graph
+import matplotlib.pyplot as plt
+# load dataset
+dataset = read_csv(new_url, header=0, index_col=0)
+values = dataset.values
+# specify columns to plot
+groups = [0, 1, 2, 3, 5, 6, 7]
+i = 1
+# plot each column
+plt.figure()
+for group in groups:
+    plt.subplot(len(groups), 1, i)
+    plt.plot(values[:, group])
+    plt.title(dataset.columns[group], y=0.5, loc='right')
+    i += 1
+plt.show()
+
+
+#
