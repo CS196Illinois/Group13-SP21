edfb4bbbce4d91df3480323c5636b54644a52054
Ayush Sharma <41262335+AyushSharma173@users.noreply.github.com>
2021-04-22 18:19:13 -0500

    Update main.py
    Created a basic 'POST' request form to collect the name of the stock from the user and would use it to display stock predictions on the /stock page by tomorrow/

--- Project/Backend/main.py
+++ Project/Backend/main.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+def Home():
+    return render_template('home.html')
+
+@app.route('/services')
+def services():
+    return render_template('forms.html')
+
+@app.route('/forms', methods = ['GET', 'POST'])
+def forms():
+    if request.method == 'POST':
+        stock_name = request.form('stock_name')
+        return render_template('/stock/' + stock_name)
+    return render_template('forms.html')
